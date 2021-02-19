07f1decb4f077823e36716b3ab64bcd2f08c34ae
Yegor <yegormostovoy@gmail.com>
2021-02-19 20:17:40 +0600

    svg

---
+++ script2.js
@@ -0,0 +1,5 @@
+let insideOfScreen = document.getElementById("button")
+
+insideOfScreen.addEventListener("click", () => {
+document.getElementById("textbox").innerText = "You've clicked on the button"
+})
