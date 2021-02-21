e5868a7ac8d6d4601186c72e03710c4bb4b69c70
yegormos <78471638+yegormos@users.noreply.github.com>
2021-02-21 19:27:51 +0600

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,5 +1,12 @@
 let insideOfScreen = document.getElementById("button")
+let appleLogo = document.getElementById("Apple")
 
 insideOfScreen.addEventListener("click", () => {
-document.getElementById("textbox").innerText = "You've clicked on the button"
+  if(appleLogo.style.visibility == "hidden") {
+      appleLogo.style.visibility = "visible";
+
+  }else {
+      appleLogo.style.visibility = "hidden";
+
+  }
 })
